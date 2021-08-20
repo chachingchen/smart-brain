4e20fb8bcfcec5855e7126d534837ee6bc315a2f
Gina.Chen/陳靓 <Gina.Chen@JMC.LOCAL>
2021-08-20 15:49:26 +0800

    upload img v1

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,6 +11,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 				<div className='form center pa4 br3 shadow-5'>
 					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
 					<button className='w-30 grow f4 link ph3 pw2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+					<input type="file" />
 				</div>
 			</div>
       	</div>
