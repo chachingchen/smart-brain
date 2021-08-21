9c672e598f68bf38ac6ba2cd6a7e098869702187
m0611104 <m0611104@gcloud.csu.edu.tw>
2021-08-21 09:42:46 +0800

    upload img br tag v8

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -8,9 +8,11 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
 				{'This magic brain will detect faces in your pictures.Give it a try!'}
 			</p>
 			<div className='center'>
-				<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
-				<input type="file" />	
-				<button className='w-30 grow f4 link ph3 pw2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+				<div className='form center pa4 br3 shadow-5'>
+					<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
+					<input type="file" />	
+					<button className='w-30 grow f4 link ph3 pw2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
+				</div>
 			</div>
       	</div>
 
